0517acc8ac5ab756b8933f378d48857521efe71f
JuanCarlos <vagrant@homestead>
2018-03-27 22:07:58 +0000

    ..

--- src/TimezonesController.php
+++ src/TimezonesController.php
@@ -4,7 +4,6 @@ namespace Mixtlisoft\Timezones;
 
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Log;
 
 
 class TimezonesController extends Controller
@@ -12,7 +11,6 @@ class TimezonesController extends Controller
 
     public function index($timezone = NULL)
     {
-        Log::error("Ejecutando timezone...");
         $current_time = ($timezone)
             ? Carbon::now(str_replace('-', '/', $timezone))
             : Carbon::now();
